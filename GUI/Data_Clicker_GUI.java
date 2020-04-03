57688ff2d6f70e2c33bf784acc2c8863442e2864
Jamison <pflanzendrache@gmx.de>
2020-04-03 16:29:14 +0100

    gui 1.0 first try

---
+++ GUI/Data_Clicker_GUI.java
@@ -0,0 +1,22 @@
+package GUI;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class Data_Clicker_GUI extends Composite {
+
+	/**
+	 * Create the composite.
+	 * @param parent
+	 * @param style
+	 */
+	public Data_Clicker_GUI(Composite parent, int style) {
+		super(parent, style);
+
+	}
+
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
+
+}
